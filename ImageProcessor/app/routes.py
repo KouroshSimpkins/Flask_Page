376db51441f25f48684a8afa6fbe718e306e0870
Kourosh Simpkins <kouroshsimpkins@gmail.com>
2020-03-02 16:32:11 +0000

    Created a Sign In page

--- ImageProcessor/app/routes.py
+++ ImageProcessor/app/routes.py
@@ -1,6 +1,26 @@
+from flask import render_template
 from app import app
+from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, Flask!"
+    user = {'username': 'Kourosh'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in London'
+        },
+        {
+            'author': {'username': 'Jane'},
+            'body': 'The Avengers movie was pretty good!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
